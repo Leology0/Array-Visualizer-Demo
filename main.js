6239706516880fc5deec47bcf1edd78bf80c785d
Leology0 <leology42@gmail.com>
2022-09-14 09:57:56 -0600

    changed

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hi");
+// Array Visualizer
+
+// HTML variables
+let containerEl = document.getElementById("container");
+
+// Global Variable
+let myData = [400, 500, 250, 550, 100, 300]
+
+// Display data
+drawArray();
+
+function drawArray() {
+    let outputStr = ""
+    for (let i =0; i < myData.length; i++) {
+        outputStr += `<div>${myData[i]}</div>`
+
+    }
+    containerEl.innerHTML = outputStr;
+}
