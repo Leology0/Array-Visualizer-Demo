1de57abb4799f8b89c3d9b29049e441aa5de2b62
Leology0 <leology42@gmail.com>
2022-09-15 09:42:44 -0600

    change

--- main.js
+++ main.js
@@ -4,7 +4,11 @@
 let containerEl = document.getElementById("container");
 
 // Global Variable
-let myData = [400, 500, 250, 550, 100, 300]
+let myData = []
+for (let n = 600; n >= 10; n-= 10) {
+    myData.push(Math.random() * 600);
+}
+console.log(myData);
 
 // Display data
 drawArray();
@@ -12,8 +16,37 @@ drawArray();
 function drawArray() {
     let outputStr = ""
     for (let i =0; i < myData.length; i++) {
-        outputStr += `<div>${myData[i]}</div>`
+        outputStr += `<div style="height:${myData[i]}px"></div>`
 
     }
     containerEl.innerHTML = outputStr;
+}
+
+// key event stuff
+document.addEventListener("keydown", keydownHandler)
+
+function keydownHandler(event) {
+    // if (event.keyCode === 38) {
+        // Up arrow - add 600 to the index
+    //     let middleIndex = Math.floor(myData.length / 2);
+    //     myData.splice(middleIndex, 0, Math.random() * 600);
+    // } else if (event.keyCode === 40) {
+    //     let middleIndex = Math.floor(myData.length / 2);
+    //     myData.splice(middleIndex, 1);
+    // }
+
+    // if (event.keyCode === 38) {
+    //     // Up arrow - Push value to the end of the array
+    //     for (let i =0; i < myData.length; i++) {
+    //         myData[i] += 10;
+    //     }
+    // } else if (event.keyCode === 40) {
+    //     let poppedValue = myData.pop();
+    //     console.log(poppedValue)
+    // }
+
+    
+    // myData.push(Math.random() * 600);
+    // redraw array
+    drawArray();
 }
